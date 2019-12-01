5effbf0ba2d3d0d7bb91693997329a979954f5e3
Eric Richardson <ejricha+github@gmail.com>
2019-12-01 11:11:51 -0500

    Don't include superfluous headers

--- ranges/Ranges.h
+++ ranges/Ranges.h
@@ -8,7 +8,6 @@
 
 #if RANGES_V3
 #define RANGES_ENABLED
-#include <range/v3/algorithm.hpp>
 #include <range/v3/all.hpp>
 namespace cppmaryland {
 	namespace Ranges = ranges;
@@ -24,14 +23,7 @@ namespace cppmaryland {
 #elif RANGES_CMCSTL2
 #define RANGES_ENABLED
 #include <experimental/ranges/algorithm>
-#include <experimental/ranges/concepts>
-#include <experimental/ranges/functional>
-#include <experimental/ranges/iterator>
-#include <experimental/ranges/memory>
-#include <experimental/ranges/random>
 #include <experimental/ranges/range>
-#include <experimental/ranges/type_traits>
-#include <experimental/ranges/utility>
 namespace cppmaryland {
 	namespace Ranges = std::experimental::ranges;
 } // namespace cppmaryland
