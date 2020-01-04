f12563e0d12be5cca52751ccc2ab7925b37581f6
Eric Richardson <ejricha+github@gmail.com>
2020-01-03 23:05:34 -0500

    Actually add some tests of concepts

--- concepts/src/Examples.cpp
+++ concepts/src/Examples.cpp
@@ -4,9 +4,10 @@
 
 #include <iostream>
 #include <string>
+#include <type_traits>
 
 // Include the concepts from the CMCSTL2 library
-//#include <stl2/concepts.hpp>
+#include <stl2/concepts.hpp>
 
 // Simple Base and Derived classes
 #include "BaseClass.hpp"
@@ -14,9 +15,13 @@
 
 namespace cppmaryland {
 
-// Ignore conversion errors for a while
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wfloat-conversion"
+// Namespace alias for concepts
+namespace Concepts = std::experimental::ranges::v1;
+
+// Define a concept to see if a type is a pointer
+template <typename T>
+	concept IsPointer =
+	std::is_pointer_v<T>;
 
 // Static string for testing
 const static std::string GlobalString { "CppMaryland" };
@@ -45,19 +50,20 @@ void FunctionsConcrete()
 	// int
 	TestConcrete(101);
 
-	// double is cast to an int
-	TestConcrete(102.222);
+	// double produces a compile error:
+	// error: conversion from ‘double’ to ‘int’ changes value from ‘1.0222199999999999e+2’ to ‘102’ [-Werror=float-conversion]
+	//TestConcrete(102.222);
 
 	// string produces a compile error:
 	// error: invalid conversion from ‘const char*’ to ‘int’ [-fpermissive]
 	//TestConcrete("one hundred three");
 
-	// BaseClass, by reference and pointer
+	// BaseClass, by value and pointer
 	BaseClass base("base");
 	TestConcrete(base);
 	TestConcrete(&base);
 
-	// DerivedClass, by reference and pointer
+	// DerivedClass, by value and pointer
 	DerivedClass derived("derived", 104);
 	TestConcrete(derived);
 	TestConcrete(&derived);
@@ -99,12 +105,12 @@ void FunctionsTemplate()
 	// string, uses the default template
 	TestTemplate("two hundred three");
 
-	// BaseClass, by reference and pointer
+	// BaseClass, by value and pointer
 	BaseClass base("base");
 	TestTemplate(base);
 	TestTemplate(&base);
 
-	// DerivedClass, by reference and pointer
+	// DerivedClass, by value and pointer
 	DerivedClass derived("derived", 204);
 	TestTemplate(derived);
 	TestTemplate(&derived);
@@ -113,15 +119,52 @@ void FunctionsTemplate()
 }
 
 // Tests of functions with concepts
+template <typename T>
+void TestConcepts(T t)
+{
+	std::cout << "  TestConcepts([UNCONSTRAINED] = " << t << ")\n";
+}
+template <typename T>
+	requires Concepts::integral<T>
+void TestConcepts(T t)
+{
+	std::cout << "  TestConcepts([integral] = " << t << ")\n";
+}
+template <typename T>
+	requires Concepts::floating_point<T>
+void TestConcepts(T t)
+{
+	std::cout << "  TestConcepts([floating_point] = " << t << ")\n";
+}
+template <typename T>
+	requires IsPointer<T>
+void TestConcepts(T t)
+{
+	std::cout << "  TestConcepts([IsPointer] = " << *t << ")\n";
+}
 void FunctionsConcepts()
 {
 	std::cout << "FunctionsConcepts:\n";
-	/*
-	TestConcepts(7);
-	DerivedClass derived("derived", 7);
+
+	// int
+	TestConcepts(301);
+
+	// double
+	TestConcepts(302.222);
+
+	// string
+	TestConcepts("three hundred three");
+
+	// BaseClass, by value and pointer
+	BaseClass base("base");
+	TestConcepts(base);
+	TestConcepts(&base);
+
+	// DerivedClass, by value and pointer
+	DerivedClass derived("derived", 304);
 	TestConcepts(derived);
 	TestConcepts(&derived);
-	*/
+
 	std::cout << std::endl;
 }
 
@@ -131,6 +174,27 @@ void FunctionsConcepts()
 // Templated function without concepts
 template <typename UnconfirmedInt>
 char AtIndex(const UnconfirmedInt i)
+{
+	return GlobalString[i];
+}
+
+// Long form of concepts
+template <typename T>
+	requires Concepts::integral<T>
+char AtIndexConstrained(const T i)
+{
+	return GlobalString[i];
+}
+
+// Short form of concepts
+template <Concepts::integral T>
+char AtIndexConstrainedShort(const T i)
+{
+	return GlobalString[i];
+}
+
+// Even shorter form of concepts
+char AtIndexConstrainedShorter(const Concepts::integral i)
 {
 	return GlobalString[i];
 }
@@ -145,25 +209,39 @@ void SimpleTests()
 	}
 	std::cout << "\n";
 
+	// Try both an integer and a floating point number as the index
+	const size_t i = 3;
+	const float f = 2.2f;
+
+	// Unconstrained templates
+
 	// This compiles fine
-	size_t i = 3;
 	std::cout << "AtIndex(" << i << ") = " << AtIndex(i) << "\n";
 
-	// This will not compile
-	/*
-	float f = 2.2f;
-	std::cout << "AtIndex(" << f << ") = " << AtIndex(f) << "\n";
-	*/
-}
+	// This will not compile:
+	//std::cout << "AtIndex(" << f << ") = " << AtIndex(f) << "\n";
+	// error: conversion from ‘float’ to ‘std::__cxx11::basic_string<char>::size_type’ {aka ‘long unsigned int’} may change value [-Werror=float-conversion]
 
-/*
-CPP_template(class Iter, class Sent, class Comp = ...some_default...)
-	(requires sentinel_for<Sent, Iter>)
-void my_algorithm(Iter first, Sent last, Comp comp = Comp{})
-{
-	// ...
+	// Use concepts to constrain templates
+	
+	// Normal form
+	std::cout << "AtIndexConstrained(" << i << ") = " << AtIndexConstrained(i) << "\n";
+	//std::cout << "AtIndexConstrained(" << f << ") = " << AtIndexConstrained(f) << "\n";
+	// error: cannot call function ‘char cppmaryland::AtIndexConstrained(T) [with T = float]’
+	
+	// Short form
+	std::cout << "AtIndexConstrainedShort(" << i << ") = " << AtIndexConstrainedShort(i) << "\n";
+	//std::cout << "AtIndexConstrainedShort(" << f << ") = " << AtIndexConstrainedShort(f) << "\n";
+	// error: cannot call function ‘char cppmaryland::AtIndexConstrainedShort(T) [with T = float]’
+	
+	// Shorter form
+	std::cout << "AtIndexConstrainedShorter(" << i << ") = " << AtIndexConstrainedShorter(i) << "\n";
+	//std::cout << "AtIndexConstrainedShorter(" << f << ") = " << AtIndexConstrainedShorter(f) << "\n";
+	// error: cannot call function ‘char cppmaryland::AtIndexConstrainedShorter(auto:1) [with auto:1 = float]’
+	
+	// Prevent warning/error about unused variable
+	(void)f;
 }
-*/
 
 // Run the tests for the following functions
 void RunTests()
@@ -179,7 +257,6 @@ void RunTests()
 	std::cout << std::endl;
 }
 
-#pragma GCC diagnostic pop
 } // namespace cppmaryland
 
 // Run all the example functions
