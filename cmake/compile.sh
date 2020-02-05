12d76591806272681052d6f74bfb44485486a3fc
Eric Richardson <ejricha+github@gmail.com>
2020-02-04 20:56:33 -0500

    Also create graphs when building

--- cmake/compile.sh
+++ cmake/compile.sh
@@ -3,16 +3,27 @@
 # Binary to run
 EXE="cyclic_dependencies/main/test_cycle"
 
+# Source the common functions
+SCRIPTS="../.external/scripts"
+source $SCRIPTS/bash/common.sh
+
 # Try both static and shared libraries
 for SHARED in OFF ON
 do
 	B="build_ninja_shared_${SHARED}"
 	echo "[$B]"
+	
+	# Create and enter the build directory
 	mkdir -p $B
 	cd $B
-	cmake -G"Unix Makefiles" -D"BUILD_SHARED_LIBS=${SHARED}" ..
-	make
-	$EXE
+
+	# Build and run the application
+	RUN cmake -G"Unix Makefiles" -D"BUILD_SHARED_LIBS=${SHARED}" ..
+	RUN make
+	RUN $EXE
+
+	# Produce graphs
+	../$SCRIPTS/bash/graph_dependencies.sh
 
 	cd -
 	echo
