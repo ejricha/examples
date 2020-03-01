b72f7d41a18ecbe59080d3df3425c526b4412b24
Eric Richardson <ejricha+github@gmail.com>
2020-03-01 12:40:37 -0500

    Add script to graph dependencies

---
+++ cmake/link_types/compile.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Logs go next to this script
+TOPDIR=`dirname $0`
+
+# Source the common functions
+SCRIPTS="../../.external/scripts"
+source $SCRIPTS/bash/common.sh
+
+# Run for the following generators
+declare -A GENERATOR
+GENERATOR[ninja]="Ninja"
+
+# More variables
+CMAKE="cmake"
+
+# The main function
+main()
+{
+	# Create the build directories,
+	#  and run the initial CMake
+	clean_cmake
+}
+
+# Run initial cmake
+clean_cmake()
+{
+	echo "clean_cmake()"
+
+	# For all the generators
+	for G in ${!GENERATOR[@]}
+	do
+		S=${GENERATOR[$G]}
+		B="build_${G}"
+		echo "[$B via $S]"
+
+		# Create and enter the build directory
+		mkdir -p $B
+		cd $B
+
+		# Generate the CMake files
+		RUN $CMAKE -G"$S" ..
+
+		# Produce graphs in the build dir
+		RUN ../$SCRIPTS/bash/graph_dependencies.sh
+
+		cd -
+		echo
+	done
+}
+
+# Call the main function
+main "$@"
